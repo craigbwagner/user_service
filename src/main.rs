791d3bb82a225370658547bde2c2a89e1ea57288
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-03 17:00:12 -0500

    add route /health and a simple controller function for it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("User service is running")
 }
