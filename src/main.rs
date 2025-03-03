10a2845117c2beb74d12a3e0be70a051fd8ef70f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-03 17:06:29 -0500

    add dotenvy and connection to database

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use dotenvy::dotenv;
+use sqlx::{Pool, Postgres};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok(); //loads environment variables
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    //create a connection pool
+    let pool = Pool::<Postgres>::connect(&database_url)
+        .await
+        .expect("Failed to create database connection pool");
+
+    println!("Connected to PostgreSQL");
+
     HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
         .bind("127.0.0.1:8080")?
         .run()
